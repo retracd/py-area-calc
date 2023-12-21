58a701bb0d9c1fef03b288998f3a45c17880ba47
retracd <29313219+retracd@users.noreply.github.com>
2023-12-21 16:04:25 -0600

    commented some code

--- area_calc.py
+++ area_calc.py
@@ -1,18 +1,22 @@
+### Python Area Calculator
+### Brent Mayes
+###
+
 from art import *
 from math import pi
 
-def splashText():
+def splashText(): # uses the art library for fancy ascii art
 	AreaText = text2art("AREA",font="alpha")
 	print(AreaText)
 	CalculatorText = text2art("                 calculator",font="chunky")
 	print(CalculatorText)
 
-def menu(funcDict):
+def menu(funcDict): # function select menu
 	print("\n\nFunction Select")
-	for funcNum in funcDict.keys():
+	for funcNum in funcDict.keys(): # iterates through the function dictionary for user display
 		print(f"	{funcNum}) {funcDict.get(funcNum)[0]}")
 	
-	funcDict.get(selFunc := input("\nWhich function do you want? "), lambda: "Invalid")[1]()
+	funcDict.get(selFunc := input("\nWhich function do you want? "), lambda: "Invalid")[1]() # calls function user picks
 
 ### AREA FUNCTIONS ###
 def rectA():
@@ -29,15 +33,16 @@ def trapA():
 ######################
 
 def main():
+	# the dictionary of all the area functions, in format num | english name | function name
 	theFuncDict = { "1" : ["Rectangle", rectA],
 			"2" : ["Circle", circleA],
 			"3" : ["Triangle", triA],
 			"4" : ["Trapezoid", trapA],
 			"9" : ["Exit", exit], }
 
-	splashText()
+	splashText() # calls the splash text visible on opening of program
 	
-	while True:
+	while True: # while true loop used to always return to the function select menu unless the exit function is called
 		menu(theFuncDict)
 
 main()
